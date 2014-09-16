bb6ffc35d2d34a7a9642b97f3e9114800f9fb020
MrBmikhael <bishoymikhael@gmail.com>
2014-09-16 12:05:23 -0700

    Uploaded Project

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnArtist = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnAlbum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnYear = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnComment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnGenre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.volControl = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnFile,
+            this.columnTitle,
+            this.columnArtist,
+            this.columnAlbum,
+            this.columnYear,
+            this.columnComment,
+            this.columnGenre});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(660, 494);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnFile
+            // 
+            this.columnFile.Text = "File";
+            // 
+            // columnTitle
+            // 
+            this.columnTitle.Text = "Title";
+            this.columnTitle.Width = 255;
+            // 
+            // columnArtist
+            // 
+            this.columnArtist.Text = "Artist";
+            // 
+            // columnAlbum
+            // 
+            this.columnAlbum.Text = "Album";
+            // 
+            // columnYear
+            // 
+            this.columnYear.Text = "Year";
+            // 
+            // columnComment
+            // 
+            this.columnComment.Text = "Comment";
+            // 
+            // columnGenre
+            // 
+            this.columnGenre.Text = "Genre";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addFileToolStripMenuItem,
+            this.deleteSelectedToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 48);
+            // 
+            // addFileToolStripMenuItem
+            // 
+            this.addFileToolStripMenuItem.Name = "addFileToolStripMenuItem";
+            this.addFileToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.addFileToolStripMenuItem.Text = "Add File";
+            // 
+            // deleteSelectedToolStripMenuItem
+            // 
+            this.deleteSelectedToolStripMenuItem.Name = "deleteSelectedToolStripMenuItem";
+            this.deleteSelectedToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.deleteSelectedToolStripMenuItem.Text = "Delete Selected";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Controls.Add(this.volControl);
+            this.panel1.Location = new System.Drawing.Point(12, 512);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 51);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(327, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(246, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "[]";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(165, 3);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = ">";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(84, 3);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "| |";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(3, 3);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "<<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // volControl
+            // 
+            this.volControl.Location = new System.Drawing.Point(553, 3);
+            this.volControl.Name = "volControl";
+            this.volControl.Size = new System.Drawing.Size(104, 42);
+            this.volControl.TabIndex = 0;
+            this.volControl.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 575);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.TrackBar volControl;
+        private System.Windows.Forms.ColumnHeader columnFile;
+        private System.Windows.Forms.ColumnHeader columnTitle;
+        private System.Windows.Forms.ColumnHeader columnArtist;
+        private System.Windows.Forms.ColumnHeader columnAlbum;
+        private System.Windows.Forms.ColumnHeader columnYear;
+        private System.Windows.Forms.ColumnHeader columnComment;
+        private System.Windows.Forms.ColumnHeader columnGenre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteSelectedToolStripMenuItem;
+    }
+}
+
---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer Player;
+        public Form1()
+        {
+            InitializeComponent();
+            Player = new WMPLib.WindowsMediaPlayer();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            //Player.controls.previous();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Player.controls.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (Player.playState == WMPPlayState.wmppsPaused)
+            {
+                Player.controls.play();
+            }
+            else
+            {
+                Player.controls.pause();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Player.controls.stop();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //Player.controls.next();
+        }
+    }
+}
